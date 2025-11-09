34acb428446c272414f01c1c786db3089cc1c28c
Afsana Zaman <Azaman6489@conestogac.on.ca>
2025-11-09 18:05:26 -0500

    Green: Implemented minimal code to pass Player1 wins test.

--- RpsLib/rps.c
+++ RpsLib/rps.c
@@ -2,6 +2,8 @@
 #include <string.h>
 
 const char* analyzeRPS(const char* player1, const char* player2) {
+    if (strcmp(player1, "Rock") == 0 && strcmp(player2, "Scissors") == 0)
+        return "Player1";
     
     return "Invalid";
 }
