f4940ea21a1561220e08ffeaec366efe8123f487
Afsana Zaman <Azaman6489@conestogac.on.ca>
2025-11-09 20:29:27 -0500

    Green: Implemented logic to pass test for Invalid Input.

--- RpsLib/rps.c
+++ RpsLib/rps.c
@@ -8,6 +8,9 @@ const char* analyzeRPS(const char* player1, const char* player2) {
         return "Player2";
     if (strcmp(player1, player2) == 0)
 		return "Draw";
-    
-    return "Temp";
+    if ((strcmp(player1, "Rock") != 0 && strcmp(player1, "Paper") != 0 && strcmp(player1, "Scissors") != 0) ||
+        (strcmp(player2, "Rock") != 0 && strcmp(player2, "Paper") != 0 && strcmp(player2, "Scissors") != 0))
+        
+        return "Invalid";
+
 }
