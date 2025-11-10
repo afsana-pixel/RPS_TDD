4ba039745c8cfdbcae1a57114e63765abb1c4f40
Afsana Zaman <Azaman6489@conestogac.on.ca>
2025-11-09 20:48:58 -0500

    Blue: Refactored code, all tests passing, clean version.

--- RpsLib/rps.c
+++ RpsLib/rps.c
@@ -2,15 +2,21 @@
 #include <string.h>
 
 const char* analyzeRPS(const char* player1, const char* player2) {
-    if (strcmp(player1, "Rock") == 0 && strcmp(player2, "Scissors") == 0)
-        return "Player1";
-    if (strcmp(player1, "Scissors") == 0 && strcmp(player2, "Rock") == 0)
-        return "Player2";
-    if (strcmp(player1, player2) == 0)
-		return "Draw";
+    
+    //Invalid Input
     if ((strcmp(player1, "Rock") != 0 && strcmp(player1, "Paper") != 0 && strcmp(player1, "Scissors") != 0) ||
         (strcmp(player2, "Rock") != 0 && strcmp(player2, "Paper") != 0 && strcmp(player2, "Scissors") != 0))
-        
         return "Invalid";
+
+    //Draw
+    if (strcmp(player1, player2) == 0)
+        return "Draw";
+
+    //Player1 Wins
+    if (strcmp(player1, "Rock") == 0 && strcmp(player2, "Scissors") == 0)
+        return "Player1";
+
+    //Player2 Wins
+	return "Player2";
 
 }
